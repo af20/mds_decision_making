2e8e4aa9a85f0bed44adeaa30c7c7fb243029fd8
Andrea Ferrari <andrea.ferrari.192@gmail.com>
2022-01-25 21:38:11 +0100

    Commenti nel file 2

--- python/es_p2.py
+++ python/es_p2.py
@@ -3,6 +3,9 @@
     x1: xGe     x2: xKe     x3: xGt     x4: xKt     x5: xGn     x6: xKn
     y1: (yE) p1open     y2: (yT) p2open     y3: (yN) p3open
 
+    G: Ginko      K: Kola
+    E: Ethiopia   T: Tanzania   N: Nigeria
+
   objective f.
     min x1*21 + x2*22.5 + x3*22.5 + x4*24.5 + x5*23 + x6*25.5 + y1*1500 + y2*2000 + y3*3000
 
@@ -22,39 +25,76 @@
   bounds
     x1,..,x6 >= 0
 '''
-
-from pulp import *
 import pandas as pd
 
-prob = LpProblem('Model_2', LpMinimize)
-x1 = LpVariable('x1', lowBound=0, cat='Integer')
-x2 = LpVariable('x2', lowBound=0, cat='Integer')
+from pulp import *
+'''  PuLP is an LP modeler written in python. PuLP can generate MPS or LP files
+     and call GLPK[1], COIN CLP/CBC[2], CPLEX[3], GUROBI[4] and MOSEK[5] to solve linear problems.'''
+
+# Definisco una Classe 'Linear Programming Problem' con l'obiettivo di 'Minimizzare' la funzione obiettivo
+prob = LpProblem('Model_2', LpMinimize)  # Creates an LP Problem ---> LpMinimize  or  LpMaximize
+
+# Aggiungo al problema le variabili
+x1 = LpVariable('x1', lowBound=0, cat='Integer')  # Variabile Intera >= 0
+'''
+  name:     The name of the variable used in the output, e.g. 'x1'
+  upBound:  The upper bound on this variable's range.
+            Default is positive infinity.
+  lowBound: The lower bound on this variable's range.
+            Default is negative infinity,
+  cat:      Integer, Binary or Continuous(default)
+'''
+
+x2 = LpVariable('x2', lowBound=0, cat='Integer')  
 x3 = LpVariable('x3', lowBound=0, cat='Integer')
 x4 = LpVariable('x4', lowBound=0, cat='Integer')
 x5 = LpVariable('x5', lowBound=0, cat='Integer')
 x6 = LpVariable('x6', lowBound=0, cat='Integer')
-y1 = LpVariable('y1', lowBound=0, cat='Binary')
-y2 = LpVariable('y2', lowBound=0, cat='Binary')
-y3 = LpVariable('y3', lowBound=0, cat='Binary')
+y1 = LpVariable('y1', cat='Binary')               # Variabile Binaria
+y2 = LpVariable('y2', cat='Binary')
+y3 = LpVariable('y3', cat='Binary')
 
 
 # obj function
 prob += x1*21 + x2*22.5 + x3*22.5 + x4*24.5 + x5*23 + x6*25.5 + y1*1500 + y2*2000 + y3*3000
 
-# constraints
-prob += x1 + x2 <= 425
-prob += x3 + x4 <= 400
-prob += x5 + x6 <= 750
+# constraints                     # CAPACITÀ PRODUTTIVA (max unità per Paese)
+prob += x1 + x2 <= 425            # Etiopia       425
+prob += x3 + x4 <= 400            # Tanzania      400
+prob += x5 + x6 <= 750            # Nigeria       750
+                                  
+                                  # DOMANDA, somma dei 3 Paesi
+prob += x1 + x3 + x5 >= 550       # Ginko         550
+prob += x2 + x4 + x6 >= 450       # Kola          450
 
-prob += x1 + x3 + x5 >= 550
-prob += x2 + x4 + x6 >= 450
+                                  # COSTI FISSI se viene prodotta 1+ unità
+prob += x1 + x2 - y1*425 <= 0     # Etiopia       1500
+prob += x3 + x4 - y2*400 <= 0     # Tanzania      2000
+prob += x5 + x6 - y3*750 <= 0     # Nigeria       3000
 
-prob += x1 + x2 - y1*425 <= 0
-prob += x3 + x4 - y2*400 <= 0
-prob += x5 + x6 - y3*750 <= 0
 
 # solve problem
-status = prob.solve(PULP_CBC_CMD(msg=0))
+status = prob.solve(PULP_CBC_CMD(msg=0)) # "PULP_CBC_CMD(msg=0)" comando per far sì che il solver NON stampi i log
+'''Param 
+      solver:  Optional: the specific solver to be used, defaults to the default solver.
+
+    _all_solvers = [
+        PULP_CBC_CMD,       # 1° default
+        GLPK_CMD,           # 2° default
+        COIN_CMD,           # 3° default
+        PYGLPK,
+        CPLEX_CMD,
+        CPLEX_PY,
+        GUROBI,
+        GUROBI_CMD,
+        MOSEK,
+        XPRESS,
+        COINMP_DLL,
+        CHOCO_CMD,
+        MIPCL_CMD,
+        SCIP_CMD,
+    ]
+'''
 
 # print results
 data = {
